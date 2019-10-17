f256fa400f0790baf6d03f7315fe45cb24bc2c67
Nilesh Maharjan <nileshmaharjan@Nileshs-MacBook-Air.local>
2019-10-17 16:45:26 +0545

    CRUD

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-class EventEmitter {
 
-    listeners = {}
-  
 
-    // takes event name and callback function to be executed 
-    
-    addListener(eventName, fn) {
-      this.listeners[eventName] = this.listeners[eventName] || [];
-      this.listeners[eventName].push(fn);
-      return this;
-    }
+var All = require('./lib/index');
+class EventEmitter extends All {
 
-    on(event, fn) {
-        return this.addListener(event, fn);
-      }
-
-    once(eventName, fn) {
-      
-        this.listeners[eventName] = this.listeners[eventName] || [];
-        
-        const onceWrapper = () => {
-          fn();
-          this.off(eventName, onceWrapper);
-        }
-        this.listeners[eventName].push(onceWrapper);
-        return this;
-    }
-
-    off(event, fn) {
-        return this.removeListener(event, fn);
-    }
-
-    removeListener (event, fn) {
-      let lis = this.listeners[event];
-      console.log(lis);
-      if (!lis) return this;
-      for(let i = lis.length - 1; i >= 0; i--) {
-        if (lis[i] === fn) {
-
-          lis.splice(i,1);
-          break;
-        }
-      }
-      return this;
-  }
+   listeners = {}
 
     emit(eventName, ...args) {
         let fns = this.listeners[eventName];
@@ -64,6 +24,37 @@ class EventEmitter {
   rawListeners(eventName) {
     return this.listeners[eventName];
   }
+
+
+  //   once(eventName, fn) {
+      
+  //       this.listeners[eventName] = this.listeners[eventName] || [];
+        
+  //       const onceWrapper = () => {
+  //         fn();
+  //         this.off(eventName, onceWrapper);
+  //       }
+  //       this.listeners[eventName].push(onceWrapper);
+  //       return this;
+  //   }
+
+  //   off(event, fn) {
+  //       return this.removeListener(event, fn);
+  //   }
+
+  //   removeListener (event, fn) {
+  //     let lis = this.listeners[event];
+  //     console.log(lis);
+  //     if (!lis) return this;
+  //     for(let i = lis.length - 1; i >= 0; i--) {
+  //       if (lis[i] === fn) {
+
+  //         lis.splice(i,1);
+  //         break;
+  //       }
+  //     }
+  //     return this;
+  // }
 }
 
 module.exports = EventEmitter;
